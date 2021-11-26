58714b866ea408e443e32f2c768d1b9cb8ee0816
AlexST19 <alex19904tsai@gmail.com>
2021-11-26 15:54:03 +0800

    Update 11_26

--- Unity_ASIA_GAME_20211001/Assets/Scripts/Controller2D.cs
+++ Unity_ASIA_GAME_20211001/Assets/Scripts/Controller2D.cs
@@ -9,11 +9,32 @@ public class Controller2D : MonoBehaviour
     public float speed = 3.5f;
     [Header("跳躍高度"),Range( 0 , 1500 )]
     public float jump = 300f;
+    [Header("確認地板尺寸與位移46824684")]
+    [Range(0, 1)]
+    public float CheckGroundReidend = 0.1f;
+    public Vector3 CheckGroundOffect;
+    [Header("跳躍按鍵與可跳躍圖層")]
+    public KeyCode KeyJump = KeyCode.Space;
+    public LayerMask canJumpLayer;
+
+
     #endregion
     /// <summary>
     /// 剛體元件 Rigidbody 2D
     /// </summary>
     private Rigidbody2D rig ;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color (0, 0, 0.2f, 0.3f);
+        Gizmos.DrawSphere(transform.position + 
+            transform.TransformDirection(CheckGroundOffect), CheckGroundReidend);
+    }
+
+    private void Update()
+    {
+        Flips();
+    }
 
     private void Start()
     {
@@ -29,6 +50,22 @@ public class Controller2D : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         print("玩家按鍵數值" + h );
+
+        rig.velocity = new Vector2(h * speed , rig.velocity.y);
+    }
+
+    private void Flips()
+    {
+        float h = Input.GetAxis("Horizontal");
+        if ( h < 0 )
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else if ( h > 0 )
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        
     }
     #endregion
 }
