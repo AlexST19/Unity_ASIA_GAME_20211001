ce26a30d801eab3851ec19361ce9e7e55ed4ab8e
AlexST19 <alex19904tsai@gmail.com>
2021-12-24 14:39:54 +0800

    12_24_03

--- Unity_ASIA_GAME_20211001/Assets/Scripts/Enemy.cs
+++ Unity_ASIA_GAME_20211001/Assets/Scripts/Enemy.cs
@@ -14,15 +14,29 @@ public class Enemy : MonoBehaviour
     [Header("�ؼйϼh")]
     public LayerMask layerTarget;
 
+    private Rigidbody2D rig;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = new Color(1, 0, 0, 0.3f);
         Gizmos.DrawCube(transform.position + transform.TransformDirection(v3TrackOffset), v3TrackSive);
         
     }
+
+    private void Update()
+    {
+        checkTargetInArea();
+    }
     private void checkTargetInArea()
     {
-        Physics2D.OverlapBox(transform.position + transform.TransformDirection(v3TrackOffset), v3TrackSive , 0);
+        Collider2D hit = Physics2D.OverlapBox(transform.position + transform.TransformDirection(v3TrackOffset), v3TrackSive , 0, layerTarget);
+
+        if (hit) rig.velocity = new Vector2(-speed, rig.velocity.y);
     }
 
 }
